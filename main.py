79ad4d5179ed355b2606dad5234e2d84115c771f
romarowski <romarowski@wisc.edu>
2023-03-11 18:00:09 -0500

    storage to gcs bucket

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from google.cloud import storage
 
 class SparefootSpider(scrapy.Spider):
     name = "sparefoot"
+
+    
+
     
     def start_requests(self):
         allowed_domains = ["www.sparefoot.com"]
@@ -17,35 +20,39 @@ class SparefootSpider(scrapy.Spider):
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
     
-    def parse(self, response, bucket):
+    def parse(self, response):
         now = datetime.datetime.now()
-        date_time = now.strftime("%m%d%YT%H%M%S")
+        date_time = now.strftime("%m%d%YT%H%M")
         page = response.url.replace('/','-')
 
+        #client = storage.Client('scraper-29')
+        #bucket = client.get_bucket('scraper23')
 
         filename = f'{date_time}/{page}.html'
-        blob = bucket.blob(filename)
-        blob.upload_from_file(response.body)
+        blob = self.bucket.blob(filename)
+        blob.upload_from_string(response.body)
         #Path(filename).write_bytes(response.body)
         self.log(f'Saved file {filename}')
   
         yield from response.follow_all(css='li.pagination-item a',
                                        callback=self.parse)
-
+#
 def set_up_bucket():
-    client = storage.Client()
+    client = storage.Client('scraper-29')
     bucket = client.get_bucket('scraper23')
     return bucket
 
 def main(request):
     def script(queue):
         try:
+            bucket = set_up_bucket()
             process = CrawlerProcess(settings={
                 #"FEEDS": {
 
                 }
                     )
-            process.crawl(SparefootSpider)
+            process.crawl(SparefootSpider,
+                          bucket=bucket)
             process.start()
             queue.put(None)
         except Exception as e:
@@ -54,7 +61,7 @@ def main(request):
     try:
 
         print("I was called !", uuid.uuid4())
-        bucket = set_up_bucket()
+        #bucket = set_up_bucket()
 
         queue = Queue()
         main_process = Process(target=script, args=(queue,))

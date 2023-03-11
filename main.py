62481c8a221dd61a541e3705afe83a9245bcd0da
romarowski <romarowski@wisc.edu>
2023-03-11 17:04:58 -0500

    try to store in blob

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import uuid
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from pathlib import Path
+import datetime
+from google.cloud import storage
+
 
 class SparefootSpider(scrapy.Spider):
     name = "sparefoot"
@@ -14,31 +17,45 @@ class SparefootSpider(scrapy.Spider):
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
     
-    def parse(self, response):
+    def parse(self, response, bucket):
+        now = datetime.datetime.now()
+        date_time = now.strftime("%m%d%YT%H%M%S")
         page = response.url.replace('/','-')
-        filename = f'results/{page}.html'
-        Path(filename).write_bytes(response.body)
+
+
+        filename = f'{date_time}/{page}.html'
+        blob = bucket.blob(filename)
+        blob.upload_from_file(response.body)
+        #Path(filename).write_bytes(response.body)
         self.log(f'Saved file {filename}')
   
         yield from response.follow_all(css='li.pagination-item a',
                                        callback=self.parse)
 
+def set_up_bucket():
+    client = storage.Client()
+    bucket = client.get_bucket('scraper23')
+    return bucket
+
 def main(request):
     def script(queue):
         try:
-            process = CrawlerProcess()
+            process = CrawlerProcess(settings={
+                #"FEEDS": {
+
+                }
+                    )
             process.crawl(SparefootSpider)
             process.start()
             queue.put(None)
         except Exception as e:
             queue.put(e)
 
-
-
-
     try:
 
         print("I was called !", uuid.uuid4())
+        bucket = set_up_bucket()
+
         queue = Queue()
         main_process = Process(target=script, args=(queue,))
         main_process.start()
